362bda3fd9f25cd2112ef0af6b0f432b45be2c41
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-06-27 11:05:03 -0300

    add mouse bindings to xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,33 +7,43 @@ import System.IO
 
 import Data.Monoid
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
-        { terminal    = myTerminal
-        , modMask     = myModMask
-        , borderWidth = myBorderWidth
-        , layoutHook  = myLayoutHook
-        , manageHook  = myManageHook
-        , workspaces  = myWorkspaces
-        , keys        = myKeys
-        , logHook     = dynamicLogWithPP xmobarPP
+        { terminal      = myTerminal
+        , modMask       = myModMask
+        , borderWidth   = myBorderWidth
+        , layoutHook    = myLayoutHook
+        , manageHook    = myManageHook
+        , workspaces    = myWorkspaces
+        , keys          = myKeys
+        , mouseBindings = myMouseBindings
+        , logHook       = dynamicLogWithPP xmobarPP
                             { ppOutput = hPutStrLn xmproc
                             , ppTitle  = xmobarColor "green" "" . shorten 50
                             }
 
         }
 
-myTerminal      = "rxvt-unicode"
-myModMask       = mod4Mask -- Super key
-myBorderWidth   = 1
-myWorkspaces    = ["web", "term", "code"] ++ map show [4..9]
-myLayoutHook    = avoidStruts $ layoutHook defaultConfig
-myManageHook    = manageHook defaultConfig <+> manageDocks
+myTerminal    = "rxvt-unicode"
+myModMask     = mod4Mask -- Super key
+myBorderWidth = 1
+myWorkspaces  = ["web", "term", "code"] ++ map show [4..9]
+myLayoutHook  = avoidStruts $ layoutHook defaultConfig
+myManageHook  = manageHook defaultConfig <+> manageDocks
 
-myKeys x = foldr M.delete (newKeys x) (keysToRemove x)
-newKeys x = M.union (keys defaultConfig x) (M.fromList (keysToAdd x))
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+        [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
+                                           >> windows W.shiftMaster))
+        , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+        , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
+                                           >> windows W.shiftMaster))
+        ]
+
+myKeys x       = foldr M.delete (newKeys x) (keysToRemove x)
+newKeys x      = M.union (keys defaultConfig x) (M.fromList (keysToAdd x))
 keysToRemove x =
     [ (modMask x, xK_c)
     ]
