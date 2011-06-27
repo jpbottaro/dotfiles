451248916b53b7951acfa9bbbb124027c5b2a142
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-06-27 15:40:03 -0300

    cleanup xmonad config file

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.LayoutHints
+--import XMonad.Layout.Fullscreen
 import XMonad.Util.Run
 import XMonad.Util.Run(spawnPipe)
 import System.IO
@@ -13,28 +14,35 @@ import qualified XMonad.StackSet as W
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
     xmonad $ defaultConfig
-        { terminal      = myTerminal
-        , modMask       = myModMask
-        , borderWidth   = myBorderWidth
-        , layoutHook    = myLayoutHook
-        , manageHook    = myManageHook
-        , workspaces    = myWorkspaces
-        , keys          = myKeys
-        , mouseBindings = myMouseBindings
-        , logHook       = dynamicLogWithPP xmobarPP
-                            { ppOutput = hPutStrLn xmproc
-                            , ppTitle  = xmobarColor "green" "" . shorten 50
-                            }
-
+        { terminal        = myTerminal
+        , modMask         = myModMask
+        , borderWidth     = myBorderWidth
+        , layoutHook      = myLayoutHook
+        , manageHook      = myManageHook
+        , workspaces      = myWorkspaces
+        , keys            = myKeys
+        , mouseBindings   = myMouseBindings
+--        , handleEventHook = myHandleEventHook
+        , logHook         = myLogHook xmproc
         }
 
-myTerminal    = "rxvt-unicode"
-myModMask     = mod4Mask -- Super key
-myBorderWidth = 1
-myWorkspaces  = ["web", "term", "code"] ++ map show [4..9]
-myLayoutHook  = layoutHints $ avoidStruts $ layoutHook defaultConfig
-myManageHook  = manageHook defaultConfig <+> manageDocks
+myTerminal        = "urxvt"
+myModMask         = mod4Mask
+myBorderWidth     = 1
+myWorkspaces      = ["web", "term", "code"] ++ map show [4..9]
+myLayoutHook      = layoutHints $ avoidStruts $ layoutHook defaultConfig
+myManageHook      = manageHook defaultConfig <+> manageDocks
+--myHandleEventHook = fullscreenEventHook
 
+-- Pipe to xmobar
+myLogHook h = dynamicLogWithPP xmobarPP
+                { ppOutput = hPutStrLn h
+                , ppTitle  = xmobarColor "green" "" . shorten 50
+                }
+
+
+
+-- Mouse Bindings
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
         [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
                                            >> windows W.shiftMaster))
@@ -43,15 +51,16 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
                                            >> windows W.shiftMaster))
         ]
 
+-- Key Bindings
 myKeys x       = foldr M.delete (newKeys x) (keysToRemove x)
 newKeys x      = M.union (keys defaultConfig x) (M.fromList (keysToAdd x))
 keysToRemove x =
     [ (modMask x, xK_c)
     ]
 keysToAdd x =
-    [ ((modMask x, xK_z), spawn "rxvt-unicode")
+    [ ((modMask x, xK_z), spawn "urxvt")
     , ((modMask x, xK_p), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
     , ((modMask x, xK_v), spawn "gvim; xdotool key Super+n")
     , ((modMask x, xK_f), spawn "chromium")
-    , ((modMask x, xK_d), spawn "rxvt-unicode -e mutt")
+    , ((modMask x, xK_d), spawn "urxvt -e mutt")
     ]
