ad63d93c2cae7d565c9e27dc98c29c942bc909de
Juan Pablo Bottaro <jpbottaro@gmail.com>
2014-01-10 13:59:33 -0300

    Fix keybindings on xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,19 +6,18 @@ import XMonad.Util.EZConfig
 import XMonad.Actions.CycleWS 
 
 main = xmonad $ gnomeConfig {
-          terminal          = "gnome-terminal"
+          terminal          = "x-terminal-emulator"
         , layoutHook        = (fullscreenFloat . fullscreenFull) $ smartBorders $ layoutHook gnomeConfig
         , handleEventHook   = handleEventHook gnomeConfig <+> fullscreenEventHook
         , manageHook        = fullscreenManageHook <+> manageHook gnomeConfig
         , focusFollowsMouse = False
         }
     `additionalKeys`
-        [ ((mod1Mask, xK_f), spawn "chromium-browser")
+        [ ((mod1Mask, xK_f), spawn "chromium")
         , ((mod1Mask, xK_v), spawn "gvim")
-        , ((mod1Mask, xK_a), spawn "eclipse")
-        , ((mod1Mask, xK_z), spawn "gnome-terminal")
         , ((mod1Mask, xK_p), spawn "kupfer")
-        , ((mod1Mask, xK_m), spawn "gnome-terminal -e mutt")
+        , ((mod1Mask, xK_z), spawn "x-terminal-emulator")
+        , ((mod1Mask, xK_m), spawn "x-terminal-emulator -e mutt")
         , ((mod1Mask, xK_e), prevScreen)
         , ((mod1Mask, xK_w), nextScreen)
         , ((mod1Mask .|. shiftMask, xK_e), shiftNextScreen)
