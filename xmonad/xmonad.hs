0ec81a8b5cad4bbbb2a547b3182985eff8fa4758
Juan Pablo Bottaro <jpbottaro@gmail.com>
2014-01-15 01:52:53 -0300

    Rehash all xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,25 +1,211 @@
+import System.IO
+import System.Exit
 import XMonad
-import XMonad.Config.Gnome
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
-import XMonad.Util.EZConfig
-import XMonad.Actions.CycleWS 
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
 
-main = xmonad $ gnomeConfig {
-          terminal          = "x-terminal-emulator"
-        , layoutHook        = (fullscreenFloat . fullscreenFull) $ smartBorders $ layoutHook gnomeConfig
-        , handleEventHook   = handleEventHook gnomeConfig <+> fullscreenEventHook
-        , manageHook        = fullscreenManageHook <+> manageHook gnomeConfig
-        , focusFollowsMouse = False
-        }
-    `additionalKeys`
-        [ ((mod1Mask, xK_space), spawn "kupfer")
-        , ((mod1Mask, xK_f), spawn "chromium")
-        , ((mod1Mask, xK_v), spawn "gvim")
-        , ((mod1Mask, xK_z), spawn "x-terminal-emulator")
-        , ((mod1Mask, xK_m), spawn "x-terminal-emulator -e mutt")
-        , ((mod1Mask, xK_e), prevScreen)
-        , ((mod1Mask, xK_w), nextScreen)
-        , ((mod1Mask .|. shiftMask, xK_e), shiftNextScreen)
-        , ((mod1Mask .|. shiftMask, xK_w), shiftPrevScreen)
-        ]
+myTerminal = "x-terminal-emulator"
+myWorkspaces = ["1:term","2:web","3:code","4:music","5:video"] ++ map show [6..9]
+myManageHook = composeAll
+    [ className =? "Chromium"       --> doShift "2:web"
+    , resource  =? "desktop_window" --> doIgnore
+    , className =? "Galculator"     --> doFloat
+    , className =? "Steam"          --> doFloat
+    , className =? "Gimp"           --> doFloat
+    , resource  =? "gpicview"       --> doFloat
+    , className =? "MPlayer"        --> doFloat
+    , className =? "VirtualBox"     --> doShift "4:vm"
+    , className =? "Xchat"          --> doShift "5:media"
+    , className =? "stalonetray"    --> doIgnore
+    , isFullscreen --> (doF W.focusDown <+> doFullFloat)]
+
+myBorderWidth = 1
+myNormalBorderColor  = "#7c7c7c"
+myFocusedBorderColor = "#ffb6b0"
+
+xmobarTitleColor = "#FFB6B0"
+xmobarCurrentWorkspaceColor = "#CEFFAC"
+
+
+myModMask = mod1Mask
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+  [ ((modMask, xK_z), spawn $ XMonad.terminal conf)
+  , ((modMask, xK_l), spawn "xscreensaver-command -lock")
+  , ((modMask, xK_p), spawn "kupfer")
+  , ((modMask, xK_f), spawn "chromium")
+  , ((modMask, xK_v), spawn "gvim")
+  , ((modMask .|. controlMask, xK_p), spawn "select-screenshot")
+  , ((modMask .|. controlMask .|. shiftMask, xK_p), spawn "screenshot")
+  , ((modMask .|. controlMask, xK_m), spawn "amixer -q set Master toggle")
+  , ((modMask .|. controlMask, xK_j), spawn "amixer -q set Master 10%-")
+  , ((modMask .|. controlMask, xK_k), spawn "amixer -q set Master 10%+")
+
+  --------------------------------------------------------------------
+  -- "Standard" xmonad key bindings
+  --
+
+  -- Audio previous.
+  , ((0, 0x1008FF16),
+     spawn "")
+
+  -- Play/pause.
+  , ((0, 0x1008FF14),
+     spawn "")
+
+  -- Audio next.
+  , ((0, 0x1008FF17),
+     spawn "")
+
+  -- Eject CD tray.
+  , ((0, 0x1008FF2C),
+     spawn "eject -T")
+
+  -- Close focused window.
+  , ((modMask .|. shiftMask, xK_c),
+     kill)
+
+  -- Cycle through the available layout algorithms.
+  , ((modMask, xK_space),
+     sendMessage NextLayout)
+
+  --  Reset the layouts on the current workspace to default.
+  , ((modMask .|. shiftMask, xK_space),
+     setLayout $ XMonad.layoutHook conf)
+
+  -- Resize viewed windows to the correct size.
+  , ((modMask, xK_n),
+     refresh)
+
+  -- Move focus to the next window.
+  , ((modMask, xK_Tab),
+     windows W.focusDown)
+
+  -- Move focus to the next window.
+  , ((modMask, xK_j),
+     windows W.focusDown)
+
+  -- Move focus to the previous window.
+  , ((modMask, xK_k),
+     windows W.focusUp  )
+
+  -- Move focus to the master window.
+  , ((modMask, xK_m),
+     windows W.focusMaster  )
+
+  -- Swap the focused window and the master window.
+  , ((modMask, xK_Return),
+     windows W.swapMaster)
+
+  -- Swap the focused window with the next window.
+  , ((modMask .|. shiftMask, xK_j),
+     windows W.swapDown  )
+
+  -- Swap the focused window with the previous window.
+  , ((modMask .|. shiftMask, xK_k),
+     windows W.swapUp    )
+
+  -- Shrink the master area.
+  , ((modMask, xK_h),
+     sendMessage Shrink)
+
+  -- Expand the master area.
+  , ((modMask, xK_l),
+     sendMessage Expand)
+
+  -- Push window back into tiling.
+  , ((modMask, xK_t),
+     withFocused $ windows . W.sink)
+
+  -- Increment the number of windows in the master area.
+  , ((modMask, xK_comma),
+     sendMessage (IncMasterN 1))
+
+  -- Decrement the number of windows in the master area.
+  , ((modMask, xK_period),
+     sendMessage (IncMasterN (-1)))
+
+  -- Toggle the status bar gap.
+  -- TODO: update this binding with avoidStruts, ((modMask, xK_b),
+
+  -- Quit xmonad.
+  , ((modMask .|. shiftMask, xK_q),
+     io (exitWith ExitSuccess))
+
+  -- Restart xmonad.
+  , ((modMask, xK_q),
+     restart "xmonad" True)
+  ]
+  ++
+
+  -- mod-[1..9], Switch to workspace N
+  -- mod-shift-[1..9], Move client to workspace N
+  [((m .|. modMask, k), windows $ f i)
+      | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+  ++
+
+  -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+  -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+  [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+      | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+  [
+    -- mod-button1, Set the window to floating mode and move by dragging
+    ((modMask, button1),
+     (\w -> focus w >> mouseMoveWindow w))
+
+    -- mod-button2, Raise the window to the top of the stack
+    , ((modMask, button2),
+       (\w -> focus w >> windows W.swapMaster))
+
+    -- mod-button3, Set the window to floating mode and resize by dragging
+    , ((modMask, button3),
+       (\w -> focus w >> mouseResizeWindow w))
+  ]
+
+-- Startup hook
+myStartupHook = return ()
+
+-- Run
+main = do
+  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+  xmonad $ defaults {
+      logHook = dynamicLogWithPP $ xmobarPP {
+            ppOutput = hPutStrLn xmproc
+          , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+          , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+          , ppSep = "   "
+      }
+      , startupHook = setWMName "LG3D"
+  }
+
+defaults = defaultConfig {
+    terminal           = myTerminal,
+    focusFollowsMouse  = myFocusFollowsMouse,
+    borderWidth        = myBorderWidth,
+    modMask            = myModMask,
+    workspaces         = myWorkspaces,
+    normalBorderColor  = myNormalBorderColor,
+    focusedBorderColor = myFocusedBorderColor,
+    keys               = myKeys,
+    mouseBindings      = myMouseBindings,
+    layoutHook         = avoidStruts $ smartBorders $ layoutHook defaultConfig,
+    manageHook         = manageDocks <+> myManageHook,
+    startupHook        = myStartupHook
+}
